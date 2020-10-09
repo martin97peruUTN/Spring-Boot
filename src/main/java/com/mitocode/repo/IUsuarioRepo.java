cf03e5a9b6f64132c20112242a7ab2919b50f2ea
Martin Perussini <mperussini@frsf.utn.edu.ar>
2020-10-09 15:19:51 -0300

    Clase 9: Spring Security

---
+++ src/main/java/com/mitocode/repo/IUsuarioRepo.java
@@ -0,0 +1,10 @@
+package com.mitocode.repo;
+
+import com.mitocode.model.Persona;
+import com.mitocode.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUsuarioRepo extends JpaRepository<Usuario, Integer> {
+    //Se le pone Integer aunque sea un int el persona. Por que? no hay porque
+    //Voy a test/java/AplicationTests
+}
--- src/test/java/com/mitocode/ApplicationTests.java
+++ src/test/java/com/mitocode/ApplicationTests.java
@@ -1,13 +1,33 @@
 package com.mitocode;
 
+import com.mitocode.model.Usuario;
+import com.mitocode.repo.IUsuarioRepo;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class ApplicationTests {
 
+    @Autowired
+    private IUsuarioRepo repo;
+
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
     @Test
-    void contextLoads() {
+    public void crearUsuarioTest() {
+        Usuario u = new Usuario();
+        u.setNombre("Usuario2");
+        u.setClave(encoder.encode("789"));
+        u.setId(2);
+        Usuario retorno = repo.save(u);
+
+        Assert.isTrue(retorno.getClave().equals(u.getClave()));
+        //No pude hacer lo que hizo el vago, pero esto parece que anda
     }
 
 }
